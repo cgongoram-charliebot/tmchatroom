2ec597d81baf8b5bf97c231a0741a639333d8f35
cgongoram-charliebot <cgongoram.charliebot@gmail.com>
2021-01-13 18:24:28 -0600

    Ultima actualizacion

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const http = require('http');
+const https = require("https");
 const express = require('express');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
@@ -19,8 +20,76 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const botName = 'ChatCord Bot';
 
+
+
+
+var startDemo = function () {
+  console.log("starting demo code");
+  executeHttps(function (err, data) {
+      if (err) {
+          console.log("Error in running demo code");
+      }
+      else {
+          console.log("Successfully ending demo code");
+      }
+
+  });
+}
+
+var executeHttps = function (callback) {
+  var options = {
+      hostname: "charliebotx.herokuapp.com",
+      //port: 443,
+      path: "/sayHelloSuccess/529991003098",
+      method: 'GET',
+      headers: {
+          'Content-Type': 'text/html'
+      }
+  };
+
+  var req = https.request(options, function (res) {
+      console.log("Status for API call : " + res.statusCode);
+      console.log("Headers for API call : " + JSON.stringify(res.headers));
+      res.setEncoding('utf8');
+
+      var body = '';
+
+      res.on('data', function (chunk) {
+          body = body + chunk;
+      });
+
+      res.on('end', function () {
+          console.log("Body for API call : " + body.length);
+          if (res.statusCode != 200) {
+              console.log("API call failed with response code " + res.statusCode);
+              callback("API call failed with response code " + res.statusCode, null)
+          } else {
+              console.log("Got response : " + body.length);
+              callback(null, body);
+          }
+      });
+  });
+
+  req.on('error', function (e) {
+      console.log("problem with API call : " + e.message);
+      callback(e, null);
+  });
+
+  req.end();
+}
+
+
+
+
+
+
+
+
+
+
 // Run when client connects
 io.on('connection', socket => {
+  startDemo();
   socket.on('joinRoom', ({ username, room }) => {
     const user = userJoin(socket.id, username, room);
 
@@ -35,6 +104,7 @@ io.on('connection', socket => {
       .emit(
         'message',
         formatMessage(botName, `${user.username} has joined the chat`)
+        
       );
 
     // Send users and room info
@@ -46,6 +116,7 @@ io.on('connection', socket => {
 
   // Listen for chatMessage
   socket.on('chatMessage', msg => {
+    startDemo();
     const user = getCurrentUser(socket.id);
 
     io.to(user.room).emit('message', formatMessage(user.username, msg));
@@ -53,6 +124,7 @@ io.on('connection', socket => {
 
   // Runs when client disconnects
   socket.on('disconnect', () => {
+    startDemo();
     const user = userLeave(socket.id);
 
     if (user) {
@@ -73,3 +145,11 @@ io.on('connection', socket => {
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+
+
+
+
+//startDemo();
